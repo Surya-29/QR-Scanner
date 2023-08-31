0a7a5c8ce0ed96329326c56e0f61045a146a34de
SK <suryakk001@gmail.com>
2023-08-31 17:44:49 +0530

    added styles

--- script.js
+++ script.js
@@ -34,11 +34,11 @@ function getData(encText, dayID, index) {
 
   if (checkVar == undefined) {
     textBox.innerHTML = "QR CODE INVALID &#10060";
-    textBox.style.color = "red";
+    // textBox.style.color = "red";
   } else {
     if (checkVar.includes(encText) && dayID == encText.slice(-4)) {
       textBox.innerHTML = "VALID &#10003";
-      textBox.style.color = "red";
+      // textBox.style.color = "red";
       let indOfEntry = checkVar.indexOf(encText);
       checkVar.splice(indOfEntry, 1);
       cont[encText.slice(0, -4)] = checkVar;
@@ -47,10 +47,10 @@ function getData(encText, dayID, index) {
     } else {
       if (!(dayID == encText.slice(-4))) {
         textBox.innerHTML = "Wrong day code is being used! &#10060";
-        textBox.style.color = "red";
+        // textBox.style.color = "red";
       } else {
         textBox.innerHTML = "User has already scanned! &#10060";
-        textBox.style.color = "red";
+        // textBox.style.color = "red";
       }
     }
   }
